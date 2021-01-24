bddba7833603bff7fa25822085d580aee1a05d94
shekem <shekem111@gmail.com>
2021-01-24 03:44:14 +0300

    Lesson2

---
+++ src/ru/aplana/Lesson2.java
@@ -0,0 +1,13 @@
+package ru.aplana;
+
+public class Lesson2 {
+
+    public static void main(String[] args) {
+        double num1 = 23452.112332;
+        byte num2 = Byte.MAX_VALUE;
+        System.out.println(num1);
+        System.out.println(num2);
+
+    }
+}
+
--- src/ru/aplana/Main.java
+++ src/ru/aplana/Main.java
@@ -3,8 +3,12 @@ package ru.aplana;
 public class Main {
 
     public static void main(String[] args) {
-	String HW = "Hello, World!";
-	System.out.println(HW);
+        double num1 = 1245584.23567;
+        float num = 1245584.23567f;
+        char character = 157;
+	System.out.println(num);
+    System.out.println(num1);
+    System.out.println(character);
 
     }
 }
