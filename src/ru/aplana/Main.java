66377c35d87856fede8f89abd1e23619aaf66ea8
shekem <shekem111@gmail.com>
2021-01-24 03:47:11 +0300

    Lesson1 (repeat)

--- src/ru/aplana/Main.java
+++ src/ru/aplana/Main.java
@@ -3,12 +3,8 @@ package ru.aplana;
 public class Main {
 
     public static void main(String[] args) {
-        double num1 = 1245584.23567;
-        float num = 1245584.23567f;
-        char character = 157;
-	System.out.println(num);
-    System.out.println(num1);
-    System.out.println(character);
+	String HW = "Hello, World!";
+	System.out.println(HW);
 
     }
 }
