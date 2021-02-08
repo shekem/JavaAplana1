a45fe38f55df397de7ffb192ed94f942230ba8df
shekem <shekem111@gmail.com>
2021-02-08 17:33:03 +0300

    Lesson7

---
+++ src/ru/aplana/Lesson7.java
@@ -0,0 +1,26 @@
+package ru.aplana;
+
+import java.util.Scanner;
+
+public class Lesson7 {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 2;
+        int z = 3;
+        Scanner sLength = new Scanner(System.in);
+        System.out.print("Введите размер массива: ");
+        int Length = sLength.nextInt();
+        int [] myArray = new int[Length];
+        for (int i = 0; i <  myArray.length; i++) {
+            Scanner sNum = new Scanner(System.in);
+            System.out.print("Введите элемент массива: ");
+            int Num = sLength.nextInt();
+            myArray[i] = Num;
+        }
+        for (int nums : myArray) {
+            if (nums == x || nums == y || nums == z)
+                System.out.println("Данное значение имеется в константах");
+            break;
+        }
+    }
+}
